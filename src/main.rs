c1ccc8fcd5c25752a6c786b660ed346caa611c71
Cass Unterholzner <cityboundforest@gmail.com>
2024-10-28 14:21:15 -0500

    Finishing up generation and presentation code; Need to add code to show candidates, also 16x16 breaks because of overflow; Perhaps need to develop my own algorithm for generating a solved Sudoku grid

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use eframe::egui;
 use rand::seq::SliceRandom;
-use sudokugen::{Board, BoardSize, Puzzle};
+use sudokugen::{board::CellLoc, Board, BoardSize, Puzzle};
+use egui_extras::Size;
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
@@ -51,7 +52,9 @@ struct Vardoku {
     board_size: Option<BoardSize>,
     start: bool,
     difficulty: Option<Difficulty>,
-    board: Option<Board>
+    board: Option<Board>,
+    notes: Option<Vec<Vec<(CellLoc, Vec<bool>)>>>,
+    max_digit: u8
 }
 
 impl Vardoku {
@@ -60,7 +63,9 @@ impl Vardoku {
             board_size: None,
             start: false,
             difficulty: None,
-            board: None
+            board: None,
+            notes: None,
+            max_digit: 0
         }
     }
 
@@ -71,13 +76,68 @@ impl Vardoku {
     fn text(&self, s: impl Into<String>) -> egui::RichText {
         egui::RichText::new(s).color(egui::Color32::WHITE).size(14.0)
     }
+
+    fn draw_cell_borders(&self, painter: &egui::Painter, rect: egui::Rect, row: usize, col: usize) {
+        let light_border = egui::Stroke::new(1.0, egui::Color32::DARK_GRAY);
+        let thick_border = egui::Stroke::new(2.0, egui::Color32::BLACK);
+        let rem = (self.max_digit as f32).sqrt() as usize;
+
+        let top_border = if row % rem == 0 { thick_border } else { light_border };
+        let left_border = if col % rem == 0 { thick_border } else { light_border };
+        let bottom_border = if row == (self.max_digit as usize - 1) { thick_border } else { light_border };
+        let right_border = if col == (self.max_digit as usize - 1) { thick_border } else { light_border };
+
+        painter.line_segment([rect.left_top(), rect.right_top()], top_border);
+        painter.line_segment([rect.left_bottom(), rect.right_bottom()], bottom_border);
+        painter.line_segment([rect.left_top(), rect.left_bottom()], left_border);
+        painter.line_segment([rect.right_top(), rect.right_bottom()], right_border);
+    }
 }
 
 impl eframe::App for Vardoku {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let cell_size = egui::vec2(15.0, 15.0);
+        //ctx.set_debug_on_hover(true);
+
         egui::CentralPanel::default().show(ctx, |ui| {
             if self.start {
+                ctx.set_pixels_per_point(match self.board_size {
+                    Some(BoardSize::FourByFour) => 9.0,
+                    Some(BoardSize::NineByNine) => 4.5,
+                    Some(BoardSize::SixteenBySixteen) => 4.5,
+                    None => panic!("question mark")
+                });
+                let mut grid = egui_grid::GridBuilder::new();
 
+                for _ in 0..self.max_digit {
+                    grid = grid.new_row(Size::exact(13.0)).cells(Size::exact(8.0), self.max_digit.into()).with_margin(egui::Margin::ZERO);
+                }
+
+                grid.show(ui, |mut grid| {
+                    for i in 0..self.max_digit {
+                        for j in 0..self.max_digit {
+                            if let Some(val) = self.board.as_ref().unwrap().get_at(i as usize, j as usize) {
+                                grid.cell(|ui| {
+                                    let cell = ui.allocate_exact_size(cell_size, egui::Sense::hover());
+
+                                    let cell_rect = cell.0.expand(0.5);
+                                    let painter = ui.painter();
+
+                                    painter.text(cell_rect.center(), egui::Align2::CENTER_CENTER, val.to_string(), egui::TextStyle::Body.resolve(&ui.ctx().style()), egui::Color32::WHITE);
+                                    self.draw_cell_borders(painter, cell_rect, i as usize, j as usize);
+                                });
+                            } else {
+                                grid.cell(|ui| {
+                                    let cell = ui.allocate_exact_size(cell_size, egui::Sense::hover());
+
+                                    let cell_rect = cell.0.expand(0.5);
+                                    let painter = ui.painter();
+                                    self.draw_cell_borders(painter, cell_rect, i as usize, j as usize);
+                                });
+                            }
+                        }
+                    }
+                });
             } else {
                 ui.vertical_centered(|ui| {
                     ui.label(self.header("Vardoku"));
@@ -85,19 +145,18 @@ impl eframe::App for Vardoku {
 
                     ui.horizontal(|ui| {
                         ui.add_space(340.0);
-                        egui::ComboBox::from_id_salt("board_size").selected_text(board_size_name(&self.board_size)).show_ui(ui, |ui| {
+                        egui::ComboBox::from_id_source("board_size").selected_text(board_size_name(&self.board_size)).show_ui(ui, |ui| {
                             ui.selectable_value(&mut self.board_size, Some(BoardSize::FourByFour), "4x4");
                             ui.selectable_value(&mut self.board_size, Some(BoardSize::NineByNine), "9x9");
-                            ui.selectable_value(&mut self.board_size, Some(BoardSize::SixteenBySixteen), "16x16");
+                            //ui.selectable_value(&mut self.board_size, Some(BoardSize::SixteenBySixteen), "16x16");
                         });
-    
                     });
                     
                     ui.label(self.text("Choose your difficulty"));
                     
                     ui.horizontal(|ui| {
                         ui.add_space(340.0);
-                        egui::ComboBox::from_id_salt("difficulty").selected_text(match &self.difficulty { Some(diff) => diff.to_string(), None => "".into() }).show_ui(ui, |ui| {
+                        egui::ComboBox::from_id_source("difficulty").selected_text(match &self.difficulty { Some(diff) => diff.to_string(), None => "".into() }).show_ui(ui, |ui| {
                             ui.selectable_value(&mut self.difficulty, Some(Difficulty::Easy), "Easy");
                             ui.selectable_value(&mut self.difficulty, Some(Difficulty::Medium), "Medium");
                             ui.selectable_value(&mut self.difficulty, Some(Difficulty::Hard), "Hard");
@@ -109,7 +168,10 @@ impl eframe::App for Vardoku {
                     let submit_button = ui.button("Submit");
 
                     if submit_button.clicked() {
+                        let first_instant = std::time::Instant::now();
+                        println!("{:?} - Generating puzzle...", std::time::Instant::now().duration_since(first_instant));
                         let puzzle = Puzzle::generate(self.board_size.unwrap());
+                        println!("{:?} - Grabbing board...", std::time::Instant::now().duration_since(first_instant));
                         let mut board = puzzle.solution().clone();
 
                         let max_digit = match self.board_size {
@@ -131,11 +193,22 @@ impl eframe::App for Vardoku {
                         let mut cell_order = (0..max_digit.pow(2)).map(|val| (val % max_digit, val / max_digit)).collect::<Vec<(u8, u8)>>();
                         cell_order.shuffle(&mut rand::thread_rng());
 
+                        println!("{:?} - Removing random values...", std::time::Instant::now().duration_since(first_instant));
+
                         for i in 0..((max_digit.pow(2) as f32 * ptr) as usize) {
                             board.unset(&board.cell_at(cell_order[i].0 as usize, cell_order[i].1 as usize));
                         }
 
+                        println!("{:?} - Done\nSetting up game.", std::time::Instant::now().duration_since(first_instant));
+
                         self.board = Some(board.to_owned());
+                        self.start = true;
+                        self.notes = Some((0..(max_digit as usize)).map(|row| {
+                            (0..(max_digit as usize)).map(|col| {
+                                (board.cell_at(row, col), (0..max_digit).map(|_| false).collect())
+                            }).collect()
+                        }).collect());
+                        self.max_digit = max_digit;
                     }
                 });
             }
